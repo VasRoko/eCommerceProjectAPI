980dee51bf853acbe1af0d6afcfbb1634a0504ac
Vas <vasylrokochuk@gmail.com>
2019-12-02 12:03:34 +0000

    Small refactor of domain classes

--- OnlineStore.Core/Entities/Product.cs
+++ OnlineStore.Core/Entities/Product.cs
@@ -2,18 +2,27 @@
 using System.Collections.Generic;
 using OnlineStore.Core.SharedKernel;
 
-namespace OnlineStore.Core.Entities
+namespace OnlineStore.Core.Domain.Entities
 {
     public class Product : BaseEntity
     {
         public string Title { get; set; }
         public DateTime Date { get; set; }
-        public int InStock { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public short? UnitsInStock { get; set; }
         public double Price { get; set; }
         public Guid CategoryId { get; set; }
         public string Description { get; set; }
         public ProductDetails ProductDetails { get; set; }
         public ICollection<ProductReview> ProductReviews { get; set; }
+        public bool Discontinued { get; private set; }
+
+        public void ProductDiscontinued()
+        {
+            Discontinued = true;
+        }
+
     }
 
     public class ProductDetails : BaseEntity
